019c0390d5e4760403bab926f773719b48951d1b
rishabhgarg25699 <rishabhgarg25699@gmail.com>
2020-08-01 14:36:40 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const path = require("path");
+const http = require("http");
+const app  = express();
+
+const hostname = "localhost";
+const port = 5678;
+
+const server = http.createServer(app);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(morgan("dev"));
+
+
+app.use(express.static(path.join(__dirname,"/public")));
+
+
+
+
+
+
+server.listen(port,hostname,function(){
+  console.log("server is listening at http://localhost:5678");
+})
