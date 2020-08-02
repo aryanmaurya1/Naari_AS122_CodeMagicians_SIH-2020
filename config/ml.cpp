0a79e48943a87bb993808be38cd626b69cbaa27a
Anubhav Vats <anubhav.cse1999@gmail.com>
2020-08-02 10:53:19 +0530

    hard cash to digi cash completed

--- config/ml.cpp
+++ config/ml.cpp
@@ -1,8 +1,11 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
 
 int main() {
-	cout << 50;
+	int arr[6]={10,20,50,100,200,2000};
+	srand(time(NULL));
+	int x=(rand())%6;
+	cout<<arr[x];
 	return 0;
 }
